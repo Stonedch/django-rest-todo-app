81a3fd54c3779d1fed3e252f0ba82c7e6dcdec7f
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-18 21:21:46 +0300

    feat[todo]: add new model "Task"

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
