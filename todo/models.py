c5dcdb8dc17e2dc3077ce0d239bd112c13fec3ca
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-18 21:31:24 +0300

    chore[todo]: change the description field of the Task model

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
