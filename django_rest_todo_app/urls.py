27061839b60ff5b9b50f224215ecf42ecbd0cdbb
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-18 22:06:10 +0300

    feat[todo]: add crud api for the Task model

--- django_rest_todo_app/urls.py
+++ django_rest_todo_app/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from todo import views
+
+router = routers.DefaultRouter()
+router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+
+from rest_framework import viewsets
+
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskView(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
