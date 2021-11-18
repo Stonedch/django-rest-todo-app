96b45e53e8eed48d3ddcb43879d789b82c20198e
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-18 21:44:15 +0300

    chore[todo]: update admin panel for the Task model

--- todo/admin.py
+++ todo/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+    list_filter = ('completed', )
+    search_fields = ('title', )
