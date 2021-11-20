470362a3f5cfb32b3148c70a0326b3c2224a090f
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-20 10:00:20 +0300

    feat: add "todo.user" app

--- django_rest_todo_app/django_rest_todo_app/settings.py
+++ django_rest_todo_app/django_rest_todo_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'todo',
+    'todo.user',
 ]
 
 MIDDLEWARE = [
---
+++ django_rest_todo_app/todo/user/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserConfig(AppConfig):
+    name = 'todo.user'
+    label = 'todo_user'
