4ab1939cc92cacf929d4124d8a8a3904a223c453
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-20 18:33:47 +0300

    fix[backend/todo.user]: update fields of user model

--- django_rest_todo_app/todo/user/models.py
+++ django_rest_todo_app/todo/user/models.py
@@ -24,10 +24,19 @@ class UserManager(BaseUserManager):
         if email is None:
             raise TypeError('Superusers must have an email.')
 
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-class User(AbstractBaseUser):
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
