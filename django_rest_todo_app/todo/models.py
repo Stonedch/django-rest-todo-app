7e036ad0e947ecaba9e3308ce58de4e091bb8526
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-24 12:11:15 +0300

    chore[backend/todo]: add FK user field for task model

--- django_rest_todo_app/todo/models.py
+++ django_rest_todo_app/todo/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from user.models import User
+
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
